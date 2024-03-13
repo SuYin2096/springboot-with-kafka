c03d005a9e564ff82cc331e970b217ca9e6217ae
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-03-13 16:49:22 +0630

    initial commit

---
+++ src/test/java/com/scarlettech/springbootwithkafka/SpringbootWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarlettech.springbootwithkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
